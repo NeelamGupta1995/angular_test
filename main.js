4b96273595fc819e914be26d0db5df4d50ab199c
NeelamGupta1995 <gupta.neelam.sn@gmail.com>
2017-07-20 23:12:40 -0700

    Todo's list is being displayed

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var testApp = angular.module("testApp",[]);
-testApp.controller("mainController",function($scope){
-	$scope.name="neelam";
+var ngTodo = angular.module("ngTodo",[]);
+ngTodo.controller("mainController",function($scope){
+	$scope.todos=["gym at 5 am","hey there","brush teeth after meal","push project to github","complete project till 27 july"];
+	$scope.add_work_todo=function(work){
+		$scope.todos.push(work);
+	}
+	$scope.delete_work_todo=function(work){
+		var index= $scope.todos.indexOf(work);
+		$scope.todos.splice(index,1);
+	}
 	
 });
