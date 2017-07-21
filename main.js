ef170d64be32b81ebd44c37ea4390f11b2de497a
NeelamGupta1995 <gupta.neelam.sn@gmail.com>
2017-07-20 23:41:10 -0700

    Beautified

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 var ngTodo = angular.module("ngTodo",[]);
 ngTodo.controller("mainController",function($scope){
-	$scope.todos=["gym at 5 am","hey there","brush teeth after meal","push project to github","complete project till 27 july"];
-	$scope.add_work_todo=function(work){
-		$scope.todos.push(work);
+	
+	$scope.todos=[
+		{
+			title:"complete",
+			details:"complete project till 27 july"
+		},
+		{
+			title:"gym",
+			details:"hey there"
+		},
+		{
+			title:"fun",
+			details:"gym at 5 am"
+		},
+		{
+			title:"routine",
+			details:"brush teeth after meal"
+		},
+		{
+			title:"github",
+			details:"push project to github"
+		}
+	];
+	
+	
+	$scope.add_work_todo=function(title,work){
+		$scope.todos.push("todos{title:"+title+"}");
+		$scope.todos.push("todos{details:"+work+"}");
 	}
 	$scope.delete_work_todo=function(work){
 		var index= $scope.todos.indexOf(work);
